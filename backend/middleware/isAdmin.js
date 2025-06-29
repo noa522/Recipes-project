a3b8de621306d4ddb12984aa68e21a837ee709fa
נעה <noa.engineer@gmail.com>
2025-06-29 21:13:12 +0300

    f

--- backend/middleware/isAdmin.js
+++
@@ -1,6 +0,0 @@
-exports.isAdmin = (req, res, next) => {
-  if (req.user?.role !== "admin") {
-    return res.status(403).json({ error: { message: "Admin access only" } });
-  }
-  next();
-};
--- backend/validations/categoryValidation.js
+++
@@ -1,7 +0,0 @@
-const Joi = require("joi");
-
-exports.categorySchema = Joi.object({
-  code: Joi.string().required(),
-  name: Joi.string().required(),
-  description: Joi.string().allow(""),
-});
